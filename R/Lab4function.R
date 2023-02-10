3e7ede1d34834b18b9a83f5165e4b00bb5e04663
Ignacio Yockers <ignacio.l.yocker-1@ou.edu>
2023-02-10 17:36:39 -0600

    Removed errors and notes

--- R/Lab4function.R
+++ R/Lab4function.R
@@ -3,11 +3,14 @@
 #' @param lm linear model of the data
 #' @param x a vector consisting of the dependent variable measurements in the linear model
 #'
+#'@importFrom stats fitted
+#'
 #' @return First the RSS, then the MSS, and the last value is the TSS
 #' @export
 #'
 #' @examples
-#' SS(cars.lm, mileage)
+#'\dontrun{SS(spruce.lm, spruce$Height)}
+#'
 SS <- function(lm, x) {
   yhat=fitted(lm)
   RSS=sum((x-yhat)^2)
