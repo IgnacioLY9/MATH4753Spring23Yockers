15a9eaa0de9de8081d178680f47e15fd24424f54
Ignacio Yockers <ignacio.l.yocker-1@ou.edu>
2023-03-31 11:00:46 -0600

    mycltu function based on unifrom distribution

---
+++ R/Lab8Function.R
@@ -0,0 +1,28 @@
+#' @title mycltu
+#'
+#' @param n number of trials in each sample
+#' @param iter number of samples
+#' @param a lower bound for the uniform distribution
+#' @param b upper bound for the uniform distribution
+#'
+#'@importFrom graphics hist lines
+#'@importFrom stats density dunif runif
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' mycltu(n=10, iter=1000, a=5, b=10)
+mycltu=function(n,iter,a=0,b=10){
+  y=runif(n*iter,a,b)
+  data=matrix(y,nrow=n,ncol=iter,byrow=TRUE)
+  w=apply(data,2,mean)
+  param=hist(w,plot=FALSE)
+  ymax=max(param$density)
+  ymax=1.1*ymax
+  hist(w,freq=FALSE,  ylim=c(0,ymax), main=paste("Histogram of sample mean", "\n", "sample size= ",n,sep=""),
+       xlab="Sample mean")
+  lines(density(w),col="Blue",lwd=3)
+  curve(dnorm(x,mean=(a+b)/2,sd=(b-a)/(sqrt(12*n))),add=TRUE,col="Red",lty=2,lwd=3)
+  curve(dunif(x,a,b),add=TRUE,lwd=4)
+}
